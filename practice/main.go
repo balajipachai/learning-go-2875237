9cd3470c85f012ddc2073b79fd16109fc89604ea
Balaji Shetty Pachai <balaji.pachai08@gmail.com>
2024-03-08 01:12:06 +0530

    convert string inputs to other types

--- practice/main.go
+++ practice/main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Your entered text is: ", input)
+
+	fmt.Print("Enter a number: ")
+	aNumber, _ := reader.ReadString('\n')
+	// Convert the entered string to a number using strconv package
+	aInt, err := strconv.ParseInt(strings.TrimSpace(aNumber), 10, 64)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Entered number is : ", aInt)
+
 }
